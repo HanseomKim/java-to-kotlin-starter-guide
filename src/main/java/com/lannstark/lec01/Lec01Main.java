7b9ba57416a6fb054332afac20f38192d2dbc247
hanseomkim <hanseomkim@gmail.com>
2024-01-27 10:13:22 +0900

    1강. 코틀린에서 변수를 다루는 방법

--- src/main/java/com/lannstark/lec01/Lec01Main.java
+++ src/main/java/com/lannstark/lec01/Lec01Main.java
@@ -1,16 +1,14 @@
 package com.lannstark.lec01;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Lec01Main {
 
   public static void main(String[] args) {
-    long number1 = 10L; // (1)
-    final long number2 = 10L; // (2)
+    long number1 = 10L; // (1) 가변, primitive type
 
-    Long number3 = 1_000L; // (3)
-    Person person = new Person("최태현"); // (4)
+    final long number2 = 10L; // (2) 불변
+
+    Long number3 = 1_000L; // (3) reference type
+
+    Person person = new Person("Kotlin"); // (4) 객체 인스턴스화
   }
-
 }
---
+++ src/main/java/com/lannstark/lec01/main.kt
@@ -0,0 +1,15 @@
+package com.lannstark.lec01
+
+fun main() {
+    // (1) var(variable): 가변(바꿀 수 있는 변수)
+    var number1 = 10L // var number1: Long = 10L
+
+    // (2) val(value): 불변(바꿀 수 없는 변수)
+    val number2 = 10L // val number2: Long = 20L
+
+    // (3) reference type
+    var number3 = 1_000L
+
+    // (4) 객체 인스턴스화
+    var person = Person("Kotlin")
+}
